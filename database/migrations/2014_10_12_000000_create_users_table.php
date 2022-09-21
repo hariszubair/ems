809e8b8ab708350f73a9213a0e61e9b6a7c0bcce
harisMac <haris.zubair@outlook.com>
2022-09-21 18:50:56 +0500

    database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->smallInteger('roles')->default(3);
+            $table->smallInteger('role')->default(3);
             $table->string('username');
             $table->string('last_name');
             $table->string('first_name');
